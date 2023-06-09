d0525d45207c94f40bfefca5dc4a9075093adacf
Daniel Potts <djpotts2891@gmail.com>
2023-06-09 20:52:30 +0000

    fixed bugs

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,4 +1,5 @@
 function sendMail(contactForm) {
+    emailjs.init("p6r8MY_sIvZDKjVLd");
     emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
@@ -7,9 +8,12 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response)
+            document.getElementById("myForm").reset();
+            document.getElementById("formresponse").innerHTML = `<h3>Form submitted sucessfully</h3>`;
         },
         function(error) {
-            console.log("ERROR", response)
+            console.log("ERROR", error)
+            document.getElementById("formresponse").innerHTML = `<h3>Error, please try again</h3><p>Error Details:<br>${error.text}`;
         }
     )
     return false;
